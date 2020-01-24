dbb9c4e2e7df2cc3ab218798a3fb7ae06e01960e
Estay_k <estaykargabay@gmail.com>
2020-01-25 01:41:45 +0600

    user image upload firestoreage  also database

--- app/src/main/java/com/estay/e_message/LoginActivity.kt
+++ app/src/main/java/com/estay/e_message/LoginActivity.kt
@@ -2,7 +2,10 @@ package com.estay.e_message
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.provider.ContactsContract
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity :AppCompatActivity(){
 
@@ -10,5 +13,15 @@ class LoginActivity :AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        login_button_login.setOnClickListener {
+           val email=email_edittext_login.text.toString()
+            val password=password_edittext_login.text.toString()
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener {}
+                .addOnFailureListener  {}
+            }
+back_to_register_textview.setOnClickListener {
+    finish()
+}
     }
 }
